60b1fc089c039dc4fe9b09bf25b5a72e7bdc7c74
xiaolei <xiaolei@globalroam.com>
2017-02-24 12:30:36 +0800

    imp maxdistancefinder

---
+++ src/Trains/waynumberfinder/MaxDistanceWayNumberFinder.java
@@ -0,0 +1,27 @@
+package Trains.waynumberfinder;
+
+import Trains.Route;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by xiaolei on 2017/2/24.
+ */
+public class MaxDistanceWayNumberFinder extends WayNumberFinder {
+
+
+    public MaxDistanceWayNumberFinder(Map<String, List<Route>> map) {
+        super(map);
+    }
+
+    @Override
+    protected boolean isRightWay(String nowWay, int nowLength, Route nowRoute) {
+        return nowRoute.getToTownName().equals(to);
+    }
+
+    @Override
+    protected boolean isOnLimited(String nowWay, int nowLength, Route nowRoute) {
+        return nowLength <= limited;
+    }
+}
